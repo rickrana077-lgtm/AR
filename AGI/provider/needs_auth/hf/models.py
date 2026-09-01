6623180e2fcfcdd49c593a3d3aeab53dd02623c6
Narasimha CyberDev <rickrana077@gmail.com>
2026-09-01 14:28:47 +0530

    Create models.py

---
+++ AGI/provider/needs_auth/hf/models.py
@@ -0,0 +1,16 @@
+from ....config import DEFAULT_MODEL
+
+default_image_model = "black-forest-labs/FLUX.1-dev"
+image_models = [
+    default_image_model,
+    "black-forest-labs/FLUX.1-schnell",
+]
+image_model_aliases = {
+    "flux": "black-forest-labs/FLUX.1-dev",
+    "flux-dev": "black-forest-labs/FLUX.1-dev",
+    "flux-schnell": "black-forest-labs/FLUX.1-schnell",
+    "stable-diffusion-3.5-large": "stabilityai/stable-diffusion-3.5-large",
+    "sdxl-1.0": "stabilityai/stable-diffusion-xl-base-1.0",
+    "sdxl-turbo": "stabilityai/sdxl-turbo",
+    "sd-3.5-large": "stabilityai/stable-diffusion-3.5-large",
+}
