cfc6c860a22079a909bd50902c2e3e33b97a13d5
Narasimha CyberDev <rickrana077@gmail.com>
2026-09-01 14:29:26 +0530

    Create _init_.py

---
+++ AGI/provider/needs_auth/hf/_init_.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from ...template.OpenaiTemplate import OpenaiTemplate
+from .HuggingFaceMedia import HuggingFaceMedia
+from .HuggingChat import HuggingChat
+
+
+class HuggingFace(OpenaiTemplate):
+    url = "https://huggingface.co"
+    base_url = "https://router.huggingface.co/v1"
+    login_url = "https://huggingface.co/settings/tokens"
+    working = True
+    active_by_default = True
+    quota_url = "https://huggingface.co/api/whoami-v2"
